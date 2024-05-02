b4bce94b89cb1b72a19e313ffdd97d3afd2c7a8b
Bryan Zambrano <bryan2016zambrano@gmail.com>
2024-05-02 15:26:46 -0500

    Adding Controllers and Swagger-UI

--- src/main/java/com/viamatica/autenticacion/AutenticacionApplication.java
+++ src/main/java/com/viamatica/autenticacion/AutenticacionApplication.java
@@ -3,11 +3,42 @@ package com.viamatica.autenticacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 @SpringBootApplication
 public class AutenticacionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutenticacionApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement()
+						.addList("Bearer Authentication"))
+				.components(new Components().addSecuritySchemes
+						("Bearer Authentication", createAPIKeyScheme()))
+				.info(new Info()
+						.title("Spring Boot 3 API")
+						.version("v1.0")
+						.description("API con microservicios Swagger")
+						.termsOfService("http://localhost.com")
+						.license(new License().name("VM").url("http:localhost.com")));
+
+	}
+
+	private SecurityScheme createAPIKeyScheme() {
+		return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+				.bearerFormat("JWT")
+				.scheme("bearer");
 	}
 
 }
